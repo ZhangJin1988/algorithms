506c08d02d4894f2cabe5f0421b94d6a47412673
ZhangJin1988 <315552364@qq.com>
2018-04-03 23:23:17 +0800

    fix bug

--- algorithm/src/com/algorithm/sort/InsertSort.java
+++ algorithm/src/com/algorithm/sort/InsertSort.java
@@ -24,7 +24,7 @@ public class InsertSort {
             int e = array[i];
             int j;
             //如果前面的值 比 暂存的值大 那么就把前面赋值到当前位置
-            for (j = i; j > 0 && array[j-1] > e; j--) {
+            for (j = i; j > 0 && array[j - 1] > e; j--) {
                 array[j] = array[j - 1];
             }
             //最后遍历完了
@@ -33,4 +33,87 @@ public class InsertSort {
 
         return array;
     }
+
+
+    private int[] sortTest(int[] array) {
+
+
+        return null;
+    }
+
+
+    private int[] inertSort2(int[] array) {
+
+
+        int m = array.length;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+
+        return array;
+
+
+    }
+
+
+    private int[] inertSortBetter(int[] array) {
+        int m = array.length;
+        int temp;
+        int j;
+        for (int i = 1; i < m; i++) {
+            temp = array[i];
+            j = i;
+            for (; j > 0; j--) {
+                if (array[j] <= array[j - 1]) {
+                    System.out.println("判断之前的j是" + j);
+                    array[j] = array[j-1];
+                }else if(array[j] > array[j - 1]){
+                    break;
+                }
+
+            }
+
+            System.out.println("判断之后的j是" + j);
+            array[j] = temp;
+        }
+
+
+        return array;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] ints = SortHelper.generateNearlyOrderArray(10, 3);
+        SortHelper.printArray(ints);
+
+
+        int[] ints2 = SortHelper.copyArray(ints);
+        InsertSort insertSort = new InsertSort();
+        int[] ints1 = insertSort.inertSort2(ints);
+
+        System.out.println("---------");
+        SortHelper.printArray(ints1);
+
+        System.out.println("-------");
+        SortHelper.printArray(ints2);
+
+
+        int[] ints3 = insertSort.inertSortBetter(ints2);
+        System.out.println("--------");
+        SortHelper.printArray(ints3);
+
+
+    }
+
+
 }
